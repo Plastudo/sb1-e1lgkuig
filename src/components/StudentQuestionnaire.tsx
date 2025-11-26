803cd127fff4775a96f7055c4573f4fe5053c3b7
Plastudo <plastudo4@gmail.com>
2025-11-26 20:12:07 +0000

    Update StudentQuestionnaire.tsx

--- src/components/StudentQuestionnaire.tsx
+++ src/components/StudentQuestionnaire.tsx
@@ -11,7 +11,7 @@ import { supabase } from '../lib/supabase'
 import { v4 as uuidv4 } from 'uuid'
 
 // Serviço de matching (dinâmico)
-import { getBestTutorMatches } from '../functions/BestFitTutors'
+import { getBestTutorMatches } from '../../src/Functions/BestFitTutors'
 
 //---------------- PERGUNTAS ---------------------
 const questions = [
